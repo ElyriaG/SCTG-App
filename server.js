521e6aa620a650cf713d3ecb326c99500cb4f716
ElyriaG <ElyriaG@users.noreply.github.com>
2020-01-23 23:25:49 -0600

    finishing touches

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ let path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.get('/key', function (require, response) {
-//     return key = process.env.ACCUWEATHER_API_KEY;
-// })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
